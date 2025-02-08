f4581941456d94d2b60a1bc486cec81158909261
Leonardo Carrijo <lh83712@gmail.com>
2025-02-08 14:39:14 -0300

    💄 style: Remoção de estilização de teste

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased pt-12 md:pt-14 md:bg-red-500 lg:bg-yellow-300`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased pt-12 md:pt-14`}
       >
         <Header />
         {children}
