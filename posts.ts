fde42ee01cfb2912154c7553888f759c13172182
Leonardo Carrijo <lh83712@gmail.com>
2025-02-09 18:48:09 -0300

    🗃️raw: Inserindo dados para testes

---
+++ posts.ts
@@ -0,0 +1,49 @@
+import type { Post } from "./typing";
+
+export const posts: Post[] = [
+    {
+        title: "How to create a new project in React",
+        content: "I'm trying to create a new project in React, but I'm having some trouble. Can someone help me?",
+        owner: "John Doe",
+        votesAmmount: 8,
+        answersAmmount: 19,
+        viewsAmmount: 34,
+        topics: ["React", "Frontend"],
+    },
+    {
+        title: "How to create a new project in Angular",
+        content: "I'm trying to create a new project in Angular, but I'm having some trouble. Can someone help me?",
+        owner: "Jane Doe",
+        votesAmmount: 8,
+        answersAmmount: 19,
+        viewsAmmount: 34,
+        topics: ["Angular", "Frontend"],
+    },
+    {
+        title: "How to create a new project in Vue",
+        content: "I'm trying to create a new project in Vue, but I'm having some trouble. Can someone help me?",
+        owner: "John Doe",
+        votesAmmount: 8,
+        answersAmmount: 19,
+        viewsAmmount: 34,
+        topics: ["Vue", "Frontend"],
+    },
+    {
+        title: "How to create a new project in Svelte",
+        content: "I'm trying to create a new project in Svelte, but I'm having some trouble. Can someone help me?",
+        owner: "Jane Doe",
+        votesAmmount: 8,
+        answersAmmount: 19,
+        viewsAmmount: 34,
+        topics: ["Svelte", "Frontend"],
+    },
+    {
+        title: "How to create a new project in React",
+        content: "I'm trying to create a new project in React, but I'm having some trouble. Can someone help me?",
+        owner: "John Doe",
+        votesAmmount: 8,
+        answersAmmount: 19,
+        viewsAmmount: 34,
+        topics: ["React", "Frontend"],
+    }
+]
--- typing.ts
+++ typing.ts
@@ -2,3 +2,13 @@ export type NavbarItem = {
     title: string;
     link: string;
 }
+
+export type Post = {
+    title: string;
+    content: string;
+    owner: string;
+    votesAmmount: number;
+    answersAmmount: number;
+    viewsAmmount: number;
+    topics: string[];
+}
